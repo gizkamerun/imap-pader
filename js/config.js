3524e499e2b9e8d0ba64b1769b1a6976a2a6e8fd
GIZ <gizkamerun@gmail.com>
2020-06-01 14:42:28 +0100

    add Pader info page & update nav links

--- js/config.js
+++ js/config.js
@@ -16,95 +16,91 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 
     $stateProvider
 
-      .state("index", {
-        abstract: true,
-        url: "/index",
-        templateUrl: "views/common/content.html",
-        resolve: {
-          loadPlugin: function ($ocLazyLoad) {
-            return $ocLazyLoad.load([
-              {
-                insertBefore: "#loadBefore",
-                name: "toaster",
-                files: [
-                  "js/plugins/toastr/toastr.min.js",
-                  "css/plugins/toastr/toastr.min.css",
-                ],
-              },
-            ]);
-          },
-        },
-      })
-      .state("activitymap", {
-        abstract: true,
-        url: "/activitymap",
-        templateUrl: "views/common/content.html",
-        resolve: {
-          loadPlugin: function ($ocLazyLoad) {
-            return $ocLazyLoad.load([
-              {
-                insertBefore: "#loadBefore",
-                name: "toaster",
-                files: [
-                  "js/plugins/toastr/toastr.min.js",
-                  "css/plugins/toastr/toastr.min.css",
-                ],
-              },
-            ]);
-          },
-        },
-      })
-      .state("index.main", {
-        url: "/main",
-        templateUrl: "views/main.html",
-        data: { pageTitle: "Example view" },
-      })
-      .state("index.minor", {
-        url: "/minor",
-        templateUrl: "views/minor.html",
-        data: { pageTitle: "Example view" },
-      })
-      .state("activitymap.activity", {
-        url: "/activity",
-        templateUrl: "views/activity.html",
-        data: { pageTitle: "Activities on the field" },
-      })
-      .state("activitymap.details", {
-        url: "/details",
-        templateUrl: "views/common/activity_details.html",
-        //controller : "MapCrtl",
-        data: { pageTitle: "Activity :: Details" },
-      })
-      .state("index.map", {
-        url: "/map",
-        templateUrl: "views/map.html",
-        data: { pageTitle: "Carte PADER" },
-      })
-      .state("index.regactivity", {
-        url: "/regactivity",
-        templateUrl: "views/regactivity.html",
-        data: { pageTitle: "A Propos" },
-      })
-      .state("index.valuechain", {
-        url: "/valuechain",
-        templateUrl: "views/valuechain.html",
-        data: { pageTitle: "A Propos" },
-      })
-      .state("index.aboutapp", {
-        url: "/aboutapp",
-        templateUrl: "views/aboutapp.html",
-        data: { pageTitle: "A Propos" },
-      })
-      .state("index.aboutgizpader", {
-        url: "/aboutgizpader",
-        templateUrl: "views/aboutgizpader.html",
-        data: { pageTitle: "GIZ/PADER" },
-      })
-      .state("index.legal", {
-        url: "/legal",
-        templateUrl: "views/legal.html",
-        data: { pageTitle: "Mentions legales" },
-      });
+        .state("index", {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+            resolve: {
+                loadPlugin: function($ocLazyLoad) {
+                    return $ocLazyLoad.load([{
+                        insertBefore: "#loadBefore",
+                        name: "toaster",
+                        files: [
+                            "js/plugins/toastr/toastr.min.js",
+                            "css/plugins/toastr/toastr.min.css",
+                        ],
+                    }, ]);
+                },
+            },
+        })
+        .state("activitymap", {
+            abstract: true,
+            url: "/activitymap",
+            templateUrl: "views/common/content.html",
+            resolve: {
+                loadPlugin: function($ocLazyLoad) {
+                    return $ocLazyLoad.load([{
+                        insertBefore: "#loadBefore",
+                        name: "toaster",
+                        files: [
+                            "js/plugins/toastr/toastr.min.js",
+                            "css/plugins/toastr/toastr.min.css",
+                        ],
+                    }, ]);
+                },
+            },
+        })
+        .state("index.main", {
+            url: "/main",
+            templateUrl: "views/main.html",
+            data: { pageTitle: "Example view" },
+        })
+        .state("index.minor", {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: "Example view" },
+        })
+        .state("activitymap.activity", {
+            url: "/activity",
+            templateUrl: "views/activity.html",
+            data: { pageTitle: "Activities on the field" },
+        })
+        .state("activitymap.details", {
+            url: "/details",
+            templateUrl: "views/common/activity_details.html",
+            //controller : "MapCrtl",
+            data: { pageTitle: "Activity :: Details" },
+        })
+        .state("index.map", {
+            url: "/map",
+            templateUrl: "views/map.html",
+            data: { pageTitle: "Carte PADER" },
+        })
+        .state("index.regactivity", {
+            url: "/regactivity",
+            templateUrl: "views/regactivity.html",
+            data: { pageTitle: "Communes d'intervention" },
+        })
+        .state("index.valuechain", {
+            url: "/valuechain",
+            templateUrl: "views/valuechain.html",
+            data: { pageTitle: "Chaines de valeur" },
+        })
+        .state("index.aboutapp", {
+            url: "/aboutapp",
+            templateUrl: "views/aboutapp.html",
+            data: { pageTitle: "A Propos" },
+        })
+        .state("index.aboutgizpader", {
+            url: "/aboutgizpader",
+            templateUrl: "views/aboutgizpader.html",
+            data: { pageTitle: "GIZ/PADER" },
+        })
+        .state("index.legal", {
+            url: "/legal",
+            templateUrl: "views/legal.html",
+            data: { pageTitle: "Mentions legales" },
+        });
 }
 angular
     .module('imap')
